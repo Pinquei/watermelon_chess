6640e3fbc470c17ecd80e72ccc1a7e780ca30789
Pinquei <54244281+Pinquei@users.noreply.github.com>
2021-05-24 17:36:17 +0800

    Add files via upload

--- get_aia.py
+++ get_aia.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request,jsonify
 from werkzeug import secure_filename
 import os
+import re
 from unzip_and_return import aia_to_xml
 from flask_cors import CORS, cross_origin
 from combine import combine
 import json
+
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +17,7 @@ CORS(app)
 @app.route('/upload', methods=['POST'])
 def upload():
     f = request.files['files']
-    team_number=request.form['team_name']
+    team_number = request.form['data']
     f.save(secure_filename(f.filename))
     result = aia_to_xml(secure_filename(f.filename)[0:-4],team_number)
     #os.remove(secure_filename(f.filename))
@@ -33,9 +35,59 @@ def save():
 
 @app.route('/battle',methods=['POST'])
 def battle():
-    red = request.form['red_name']
-    yellow= request.form['yellow_name']
-    exec(open('./combine_code/'+str(red)+' vs '+str(yellow)+'.py', encoding="utf-8").read(),globals())
+    getdata=request.form['pythonCodeData']
+    getdata=re.split(': |,', getdata)
+    codea=getdata[1].replace('\\n','\n')
+    codeb=getdata[3].replace('\\n','\n')
+    file_write = open('codea.txt', 'w', encoding="utf-8")
+    file_write2 = open('codeb.txt', 'w', encoding="utf-8")
+    for i in codea[2:-2]:
+        file_write.write(i)
+        if i =='\\':
+            file_write.write('\n')
+    file_write.close()
+    for i in codeb[2:-2]:
+        file_write2.write(i)
+        if i =='\\':
+            file_write2.write('\n')
+    file_write2.close()
+
+    file = open('codea.txt', 'r', encoding="utf-8")
+    file2 = open('codeb.txt', 'r', encoding="utf-8")
+    filehead = open('strategy_battle_head.txt', encoding="utf-8")
+    filemiddle = open('strategy_battle_middle.txt', encoding="utf-8")
+    filebottom = open('strategy_battle_bottom.txt', encoding="utf-8")
+    file_write = open('combine_code/' + 'battle.py', 'w', encoding="utf-8")
+    contents = file.readlines()
+    contents2 = file2.readlines()
+    contentshead = filehead.readlines()
+    contentsmiddle = filemiddle.readlines()
+    contentsbottom = filebottom.readlines()
+    for line in contentshead:
+        file_write.write(line)
+    filehead.close()
+    file_write.write('    ' + str(line))
+
+    for line in contents:
+        file_write.write('    ' + str(line))
+    file.close()
+
+    for line in contentsmiddle:
+        file_write.write(line)
+    filemiddle.close()
+
+    for line in contents2:
+        file_write.write('    ' + str(line))
+    file.close()
+
+
+    for line in contentsbottom:
+        file_write.write(line)
+    filebottom.close()
+    file_write.close()
+
+
+    exec(open('./combine_code/'+'battle.py', encoding="utf-8").read(),globals())
     with open('./combine_code/BattleReport/Report.json', 'r', encoding="utf-8") as json_file:
         data = json.load(json_file)
         return jsonify(data)
